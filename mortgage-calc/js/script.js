bfd4c3d408487a0bea53a327d483457a58c04cf2
LSMariee <sherfinski.l@stu.janesville.k12.wi.us>
2023-11-21 08:46:22 -0600

    update 11/21

--- mortgage-calc/js/script.js
+++ mortgage-calc/js/script.js
@@ -1,12 +1,19 @@
 function calc() {
-    var p = Number(document.getElementById("p").value);
+    var principal = Number(document.getElementById("principal").value);
     var intrate = Number(document.getElementById("intrate").value);
     var years = Number(document.getElementById("years").value);
     var mnthpay = Number(document.getElementById("mnthpay").value);
     var ratedec = intrate / 100;
-    var exponent = 12 * years
+    var exponent = 12 * years;
 
-    var balance = p (1 + ratedec / 12) ** exponent - (mnthpay((1 + ratedec/12) ** exponent - 1)) / (ratedec / 12);
-    msg = "Remaining Loan Balance: " + balance.toFixed(2);
-    document.getElementById('balance').innerHTML = msg;
+
+    var balance = principal * (1 + ratedec / 12) ** exponent - (mnthpay * ((1 + ratedec/12) ** exponent - 1)) / (ratedec / 12);
+    var msg = "Remaining Loan Balance: $" + balance.toFixed(2);
+    var out = document.getElementById("out");
+    out.innerHTML = msg;
+
+}
+
+function reset() {
+    document.getElementById('out').innerHTML = "";
 }
